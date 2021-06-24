2a0259b21ffea7f10d1d8e285b8794c02a957ffe
Yannick Thockler <yannick-thockler@cifacom.com>
2021-06-24 12:14:11 +0200

    ajout de jooble.php / infos sur readme

--- index.php
+++ index.php
@@ -2,3 +2,7 @@
 <p>Projet mené par la classe DWWM8 et supervisé par Julien Buabent - AFPA 2021</p>
 <br>
 <p>C'est parti !</p>
+<br>
+<h2>Groupe meteojobs </h2>
+<span>API pour jooble :</span>
+<a href="jooble.php">Tests sur l'api de jooble</a>
