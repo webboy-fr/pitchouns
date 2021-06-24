0a2aa637e071f8fb20a28a8987e30776190f79e7
Ayyoub Mehdioui <Ayyoub36@hotmail.fr>
2021-06-24 15:07:48 +0200

    modif_lien

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
     <p>Projet mené par la classe DWWM8 et supervisé par Julien Buabent - AFPA 2021</p>
     <br>
     <p>C'est parti !</p>
-    
-      <!-- lien vers welcome.html -->
-      <?= "<a  href='welcome.html'>Rapport de faisabilité</a>" ?>
+
+    <!-- lien vers welcome.html -->
+    <a href='welcome.html'>Rapport de faisabilité</a>
