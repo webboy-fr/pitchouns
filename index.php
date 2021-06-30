e49eed81b7cd5ab7dce72a734926f80752a0d92b
Tom Aznar <aznar.tom@gmail.com>
2021-06-30 14:06:57 +0200

    changement des title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <style>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap');
   </style> 
-  <title>Document</title>
+  <title>Accueil</title>
 </head>
 <?require "nav.php"?>
 <br>
